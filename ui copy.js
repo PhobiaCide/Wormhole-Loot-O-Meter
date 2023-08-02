de9b0f9d6cb2bf607e6d41de347a613e4cd8fce2
PhobiaCide <80663955+PhobiaCide@users.noreply.github.com>
2023-08-02 14:08:47 -0500

    UPDATED: Added a new file "ui copy.js" with various changes and configurations.
    - Initialized "config" object with various properties such as "clearRow", "colGroups", "rowGroups", "playerNames", "playerList", "dataValidation", "siteList", "checkboxArea", "fontColorDark", "fontColorLight", and "drifterToast".
    - Defined a "test" function that logs the hexadecimal values of "fontColorDark" and "fontColorLight".
    - Created an "onEdit" function that triggers on an edit event and performs actions based on the range of the edit.
    - Added a "stylesheet" function that returns CSS styling for the script.
    - Created an "onOpen" function that adds a custom menu to the.
    - Defined functions for creating templates and including files in the script.
    - Added functions for displaying toast messages, alerts, and update roster notifications.
    - Implemented "userExecutedUpdateRoster" function that triggers a roster update.
    - Added functions for clearing inputs and the entire sheet.
    - Included functions for collapsing row and column groups.
    - Implemented an "actionCanceled" for canceling actions.
    - Added a "doGet" function for serving the script as a app- Created a "getLastRow" function for getting the last populated row in a sheet.
    - Defined an "addToRoster" function for writing character IDs and names to sheet a information.
    -" clears inputs, pilots, and collapses all groups.
    - Created functions for collapsing all row and column groups.

---
+++ ui copy.js
@@ -0,0 +1,573 @@
+const config = {
+  clearRow: Array(19).fill([0]),
+  colGroups: [10, 15, 20],
+  rowGroups: [11, 18, 25, 32, 39],
+  playerNames: main.getRange(`E2:W2`),
+  playerList: {
+    row: 2,
+    startCol: 5,
+    endCol: 23,
+    clearColumn: Array(40).fill([0])
+  },
+  dataValidation: {
+    pilotSelectionRange: roster.getRange(`C2:C`),
+    authenticatedNamesRange: roster.getRange(`D2:D`)
+  },
+  siteList: {
+    col: 3,
+    startRow: 5,
+    endRow: 45,
+    drifterSiteList: sites
+      .getRange(2, 1, 48, 4)
+      .getDisplayValues()
+      .filter(siteData => siteData[3])
+      .map(row => row[1])
+  },
+  checkboxArea: {
+    startRow: 5,
+    startCol: 4,
+    endRow: 45,
+    endCol: 19
+  },
+  fontColorDark: main.getRange(`A1`).getFontColorObject(),
+  fontColorLight: main.getRange(`B3`).getFontColorObject(),
+  drifterToast: {
+    title: ``,
+    message: ``
+  }
+};
+
+function test() {
+  console.log(config.fontColorDark.asRgbColor().asHexString());
+  console.log(config.fontColorLight.asRgbColor().asHexString());
+}
+
+function onEdit(e) {
+
+  const { range } = e;
+  if (!range) return;
+
+  const col = range?.getColumn();
+  const row = range?.getRow();
+
+  const cols = range?.getLastColumn();
+  const rows = range?.getLastRow();
+
+  const isOneCell = (cols - col) + (rows - row) === 0; 
+
+  if (!isOneCell) return;
+
+  const isWithinPlayerNameRange = (row == config.playerList.startRow
+    && (column >= config.playerList.startCol
+    && column <= config.playerList.endCol));
+
+  const isWithinSiteListRange = col == (config.siteList.startCol
+    && (row >= config.siteList.startRow
+    && row <= config.siteList.endRow));
+
+  if (!isWithinPlayerNameRange && !isWithinSiteListRange) return;
+
+  const cell = main?.getRange(row, column);
+  const value = cell?.getValue();
+
+  if (isWithinPlayerNameRange) {
+    if (config.colGroups.includes(col)) {
+      const group = main.getColumnGroup(col, 1);
+      value
+        ? group.expand()
+        : group.collapse();
+    }
+    const selectionRange = (col == config.playerList.startCol)
+      ? roster.getRange(config.playerList.dataValidation.authenticatedNamesRange)
+      : roster.getRange(config.playerList.dataValidation.pilotSelectionRange);
+    const rule = ss.newDataValidation().requireValueInRange(selectionRange, true).build();
+    value
+      ? cell.clearDataValidations()
+      : cell.setDataValidation(rule);
+    !value && main
+      .getRange(config.siteList.startRow, col, config.siteList.endRow, 1)
+      .setValues(config.playerList.clearColumn);
+  } else if (isWithinSiteListRange) {
+    if (config.rowGroups.includes(row)) {
+      const group = main.getRowGroup(row, 1);
+      value
+        ? group.expand()
+        : group.collapse();
+    }
+    const siteName = main.getRange(row, col).getDisplayValue().toString();
+    const hasDrifter = config.input.siteList.drifterSiteList.includes(siteName);
+    if (row == 5) {
+      const [playerNames] = config.input.playerNames.getDisplayValues();
+      const newValues = [playerNames.map(name => name ? 1 : 0)].reduce((a, b) => [a, b]);
+      main.getRange(row, config.input.checkboxArea.startCol, 1, 19).setValues([newValues]);
+    } else if (row > 5) {
+      const previousValues = main.getRange(`E${row - 1}:W${row - 1}`).getDisplayValues();
+      main.getRange(`E${row}:W${row}`).setValues(previousValues);
+    }
+    if (hasDrifter) {
+      const article = (siteName === `Oruze Osobnyk`) ? an : a;
+      toast(`There is an optional Drifter Response Battleship at ${article} ${siteName}!`, `Drifter Response Battleship`, 30);
+      const rule = ss.newDataValidation().requireCheckbox(1).build();
+      const drifterCell = main.getRange(row, 4);
+      drifterCell.setDataValidation(rule);
+      drifterCell.setFontColor(config.output.fontColor);
+    }
+
+    return;
+  }
+}
+
+const stylesheet = () => `
+root {
+  --color: ${config.fontColorDark.asRgbColor().asHexString()};
+  --bg: ${config.fontColorLight.asRgbColor().asHexString()};
+}`;
+
+/**
+ *
+ */
+function onOpen(e) {
+  const ui = ss.getUi();
+
+  ui.createMenu(`⚖️ Wormhole Loot-O-Meter`)
+    .addSubMenu(
+      ui.createMenu(`📋 Roster management`)
+        .addItem(`➕ Add a pilot manually`, `manualAddPilot`)
+        .addSeparator()
+        .addItem(`📡 Update pilot roster`, `updateRoster`)
+    )
+    .addSubMenu(
+      ui.createMenu(`🎛️ Sheet utility`)
+        .addItem(`🗞 Collapse all groups`, `collapseAllGroups`)
+        //.addItem(`onOpen`, `onOpen`)
+        .addSeparator()
+        .addItem(`🧱 Load current fleet`, `loadCurrentFleet`)
+        .addSeparator()
+        .addItem(`🔲 Clear inputs`, `userExecutedClearInputs`)
+        .addItem(`💣 Clear inputs and pilots`, `userExecutedClearSheet`)
+    )
+    .addSeparator()
+    .addItem(`❓ How to use`, `helpDialog`)
+    .addToUi();
+}
+
+function createTemplateFromFile(filename, title) {
+  return HtmlService.createTemplateFromFile(filename)
+    .evaluate()
+    .setTitle(title);
+}
+
+function createTemplate(filename, title) {
+  return HtmlService.createTemplate(filename)
+    .evaluate()
+    .setTitle(title);
+}
+
+function includeFile(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+    .getContent();
+}
+
+function include(string) {
+  return HtmlService.createHtmlOutput(string)
+    .getContent();
+}
+
+/**
+ *
+ */
+function toast(message, title, time) {
+  SpreadsheetApp.getActiveSpreadsheet().toast(message, title, time);
+};
+
+/**
+ *
+ */
+function alert(title, prompt, buttons = `OK`) {
+  const ui = ss.getUi();
+  const buttonSet = buttons == `YES_NO` ? ui.ButtonSet.YES_NO : ui.ButtonSet.OK;
+
+  return ui.alert(title, prompt, buttonSet);
+};
+
+/**
+ *
+ */
+function updateRosterToast(updateCount) {
+  toast(`Roster update was successful. ${updateCount} new pilots were added to the roster.`, `Roster Update Success`, 30);
+};
+
+/**
+ *
+ */
+function userExecutedClearSheet() {
+  const ui = ss.getUi();
+  const response = alert(
+    `Warning`, `This action will reset all checkboxes and current pilot lineup.
+
+Do you wish to continue?`, ui.ButtonSet.YES_NO
+  );
+  if (response !== ui.Button.YES) {
+
+    return actionCanceled();
+  }
+  clearSheet();
+}
+
+/** Start of test code 
+
+function getCharacterId(characterName) {
+  var url = 'https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en';
+  var payload = {
+    'names': [
+      characterName
+    ]
+  };
+  var options = {
+    'method': 'post',
+    'contentType': 'application/json',
+    'payload': JSON.stringify(payload)
+  };
+  var response = UrlFetchApp.fetch(url, options);
+  var data = JSON.parse(response.getContentText());
+  var characterId = data.characters[0].id;
+  return characterId;
+}
+
+function getCharacterPortrait(characterId) {
+  var url = `https://esi.evetech.net/latest/characters/${characterId}/portrait/?datasource=tranquility`;
+  var response = cacheUrlFetchApp(url);
+  var data = JSON.parse(response.getContentText());
+  var portraitUrl = data.px128x128;
+  return portraitUrl;
+}
+
+function confirmCharacter(characterName) {
+  var characterId = getCharacterId(characterName);
+  var portraitUrl = getCharacterPortrait(characterId);
+  var html = HtmlService.createTemplateFromFile('Confirm')
+    .evaluate()
+    .setWidth(300)
+    .setHeight(300);
+  html.characterName = characterName;
+  html.portraitUrl = portraitUrl;
+  SpreadsheetApp.getUi().showModalDialog(html, 'Confirm Character');
+}
+
+function processForm(form) {
+  var characterName = form.characterName;
+  confirmCharacter(characterName);
+}
+
+End of test Code */
+
+/**
+ *
+ */
+function manualAddPilot() {
+  const title = `Manually Add A Pilot`;
+  const source = `
+    <!DOCTYPE html>
+    <html id="sidebar">
+      <head>
+        <base target="top">
+
+        <title>Add Pilot Sidebar</title>
+
+        <link rel="stylesheet" href="https://ssl.gstatic.com/docs/script/css/add-ons.css">
+        <style>
+
+          ${stylesheet}
+
+        </style>
+
+        <?= include(style) ?>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+      </head>
+      <body>
+        <!-- Below is the HTML code that defines the sidebar element structure. -->
+        <header>
+          <h1>Pilot Search</h1>
+        </header>
+        <hr>
+        <main>
+          <p>Type a pilot's name into the box below and click the "Search" button to begin the process.</p>
+
+          <form id="myForm">
+            <label for="characterName">Search:</label><br>
+            <input type="text" id="characterName" name="characterName" placeholder="Enter character name:"><br>
+            <input type="button" value="Search" onclick="submitForm()">
+          </form>
+
+          <div id="result-container">
+            <!-- Results will be displayed here -->
+          </div>
+
+        </main>
+        <!-- Enter sidebar bottom-branding below. -->
+        <hr>
+        <footer>
+          <span>
+            Made with ✨ by <a target="blank" href="https://github.com/PhobiaCide">PhobiaCide</a>.
+          </span>
+        </footer>
+        <script>
+
+          <?= include(script) ?>
+
+        </script>
+      </body>
+    </html>
+  `;
+  
+  const sidebar = createTemplate(source, title)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  ui.showSidebar(sidebar);
+}
+
+/**
+ *
+ */
+function helpDialog() {
+  const dialogTitle = ``;
+
+  const helpDialogSource = `
+  <!DOCTYPE html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <base target="top">
+    <title>🦀 Wormhole Loot-O-Meter Help</title>
+
+    <style>
+
+    ${stylesheet}
+
+    </style>
+
+    <?= include(style) ?>
+
+  </head>
+  <body>
+      <header class="info">
+        <p class="logo">🦀</p>
+        <h2>How To Use</h2>
+        <h1> Wormhole<br>Loot-O-Meter</h1>
+        <h3>Version: 2.0.0</h3>
+      </header>
+      <hr>
+      <main>
+        <section>
+          <details>
+            <summary>
+              <h2>Before You Begin:</h2>
+            </summary>
+            <ul>
+              <li>
+                You must have
+                <a href="https://workspace.google.com/u/0/marketplace/app/gesi/747777332185" target="blank" rel="noopener noreferrer">
+                  GESI
+                </a>
+                installed.
+              </li>
+              <li>
+                You must be logged in to GESI with at least one Eve Online account.
+              </li>
+              <li>
+                The character that is logged in to GESI must be the fleet boss of an active fleet in game.
+              </li>
+            </ul>
+          </details>  
+        </section>
+        <hr>
+        <section>
+          <details>
+            <summary>
+              <h2>Instructions:</h2>
+            </summary>
+            <ol>
+              <li>
+                Click "Help"
+              </li>
+              <li>
+                Give the script permission to run
+              </li>
+              <li>
+                Refresh the page
+              </li>
+              <li>
+                Navigate to <i>⚖️ Wormhole Loot-O-Meter &gt;&gt; 📋 Roster Management &gt;&gt; 📡 Update Roster</i> and allow the script to run.
+              </li>
+              <li>
+                Select the wormhole class from the drop-down menu at the top.
+              </li>
+              <li>
+                Select a site from the drop-down menu. The sites will be limited to only sites of the selected class.
+              </li>
+              <li>
+                In the same row as the site, check the box under each of the participants. If a site was selected that has an optional <i>drifter battleship</i>, a box will appear on the same row but in the "Drifter" column. Checking this box will include those drops as well.
+              </li>
+              <li>
+                Run the site.
+              </li>
+              <li>
+                The Wormhole Loot-O-Meter will track each pilot's share.
+              </li>
+            </ol>
+          </details>
+        </section>
+      </main>
+     
+      <footer>
+        <span>
+          Made with ✨ by <a target="blank" href="https://github.com/PhobiaCide">PhobiaCide</a>.
+        </span>
+      </footer>
+    </body>
+  </html>
+`
+  const html = HtmlService.createHtmlOutput(helpDialogSource)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+  const blob = HtmlService.createHtmlOutput(html)
+    .setWidth(600)
+    .setHeight(800)
+    .setTitle(dialogTitle)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  SpreadsheetApp.getActiveSpreadsheet().show(blob);
+}
+
+/**
+ * Reads the existing list of character IDs from the roster sheet.
+ * @returns {string[]} The list of character IDs.
+ */
+function getSavedIds() {
+  const rows = roster.getLastRow();
+  if (rows === 0) {
+    return [];
+  }
+  const range = roster.getRange(2, 1, rows);
+
+  return range.getDisplayValues()
+    .flat()
+    .filter(id => id.trim() !== ``);
+}
+
+/**
+ *
+ */
+function doGet() {
+  const result = HtmlService.createHtmlOutput(manualAddPilotSource()).getContent().toString();
+  console.log(typeof result);
+
+  return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.TEXT);
+}
+
+/**
+ * lastRow
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - The sheet whose rows are to be counted
+ * 
+ * @return {Number} - The number of populated rows in the spreadsheet
+ */
+function getLastRow(sheet) {
+  return sheet.getLastRow()
+}
+
+/**
+ *
+ */
+function clearInputs() {
+  console.time(`Executed clearInputs() in`);
+  const ui = SpreadsheetApp.getUi();
+  const response = alert(
+    `Warning`, `This action will reset all check boxes.
+
+    Do you wish to continue?`, ui.ButtonSet.YES_NO
+  );
+  if (response !== ui.Button.YES) return actionCanceled();
+
+  main.getRange(5, 3, 40, 19).clearContent()
+  main.getRange(5, 4, 40)
+    .clearDataValidations()
+    .removeCheckboxes()
+    .setFontColor(`white`);
+
+  collapseAllGroups();
+
+  console.timeEnd(`Executed clearInputs() in`);
+};
+
+/**
+* @summary A function that writes the character IDs and names to the roster sheet.
+* @param {string[][]} data - The list of character IDs and names to write.
+*/
+function addToRoster(data = [[]]) {
+  // if data is not an array, wrap it in two sets of square brackets else if data[0] is not an array. wrap in a single set of brackets to ensure 2 dimensional array
+  const array = !Array.isArray(data) ? [[data]] : !Array.isArray(data[0]) ? [data] : data;
+
+  // Destructure assignment to find the number of columns based on the length of array
+  const { length: rows } = array;
+  // Destructure assignment to find the number of columns based on the length of array[0]
+  const { length: cols } = array[0];
+
+  // Get range starting on row, col and ending on rows, cols and set the values to the data
+  roster.getRange(2, 1, rows, cols).setValues(data);
+};
+
+/**
+ *
+ */
+function clearPilots() {
+  console.time(`Executed clearPilots() in`)
+
+  config.playerNames.clearContent();
+  const validNames = config.dataValidation.pilotSelectionRange;
+  const rule = ss.newDataValidation().requireValueInRange(validNames, true).build();
+  range.setDataValidation(rule);
+
+  console.timeEnd(`Executed clearPilots() in`)
+};
+
+/**
+ *
+ */
+function clearSheet() {
+  console.time(`Executed clearSheet() in`);
+
+  clearInputs();
+  clearPilots();
+  collapseAllGroups();
+
+  console.timeEnd(`Executed clearSheet() in`);
+};
+
+/**
+ *
+ */
+function collapseAllGroups() {
+  collapseAllRowGroups();
+  collapseAllColumnGroups();
+};
+
+/**
+ *
+ */
+function collapseAllColumnGroups() {
+  main.collapseAllColumnGroups();
+};
+
+/**
+ *
+ */
+function collapseAllRowGroups() {
+  main.collapseAllRowGroups();
+}
+
+/**
+ *
+ */
+function actionCanceled() {
+  return toast(`Spreadsheet was not changed`, `Action canceled`, 30);
+};
